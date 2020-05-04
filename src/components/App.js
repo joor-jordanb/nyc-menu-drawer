f9c20042e21a53ffa242e44c33f1bc6529c989db
joor-jordanb <jbuzzell@jooraccess.com>
2020-05-04 16:33:57 -0400

    (wip) first commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Map from './Map';
+
+export default class AppContainer extends Component {
+  render() {
+    return (
+      <Map/>
+    )
+  }
+}
